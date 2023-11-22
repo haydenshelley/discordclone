2d8a68237d31fd4f57177607aecd8414616de5bd
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-11-22 13:11:23 -0800

    dynamic room links

--- base/views.py
+++ base/views.py
@@ -9,5 +9,10 @@ rooms = [
 def home(request):
   return render(request, 'base/home.html', {'rooms':rooms})
 
-def room(request):
-  return render(request, 'base/room.html')
+def room(request, pk):
+  room = None 
+  for i in rooms:
+    if i['id'] == int(pk):
+      room = i
+  context = {'room':room}
+  return render(request, 'base/room.html', context)
