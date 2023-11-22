ecd6984189b4b953f66dfae0da01b132cb0669b6
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-11-22 13:04:22 -0800

    move views to base/templates/base

--- base/views.py
+++ base/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 rooms = [
-  {'id': 1, 'name':'Python!'},
+  {'id': 1, 'name':'Python'},
   {'id': 2, 'name':'Ruby'},
   {'id': 3, 'name':'Javascript'},
 ]
 
 def home(request):
-  return render(request, 'home.html', {'rooms':rooms})
+  return render(request, 'base/home.html', {'rooms':rooms})
 
 def room(request):
-  return render(request, 'room.html')
+  return render(request, 'base/room.html')
