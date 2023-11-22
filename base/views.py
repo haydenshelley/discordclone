7756b6a42300eed93a1e37d9a5ea48a786370168
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-11-22 13:00:39 -0800

    room library

--- base/views.py
+++ base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+rooms = [
+  {'id': 1, 'name':'Python!'},
+  {'id': 2, 'name':'Ruby'},
+  {'id': 3, 'name':'Javascript'},
+]
+
 def home(request):
-  return render(request, 'home.html')
+  return render(request, 'home.html', {'rooms':rooms})
 
 def room(request):
   return render(request, 'room.html')
