a5d5a3c9a51039a6fe6c2e3c708fae3d46df91b1
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-11-22 12:34:48 -0800

    configure base routes

--- discordclone/urls.py
+++ discordclone/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls'))
 ]
